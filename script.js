e9c366b6d102067a23f2e3cbeb98d32f98ff6d8d
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2024-01-12 18:59:06 +0700

    finsihed

--- script.js
+++ script.js
@@ -3,20 +3,18 @@ function getComputerChoice () {
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
-function getPlayerChoice () {
-    const moves = ["rock", "paper", "scissor"];
-    let playerChoice = prompt("enter your move:");
+// function getPlayerChoice () {
+//     const moves = ["rock", "paper", "scissor"];
+//     let playerChoice = prompt("enter your move:");
     
-    while (!moves.includes(playerChoice)) {
-        alert('Wrong input');
-        playerChoice = prompt("enter your move:");
-    }
-    return playerChoice;
-}
+//     while (!moves.includes(playerChoice)) {
+//         alert('Wrong input');
+//         playerChoice = prompt("enter your move:");
+//     }
+//     return playerChoice;
+// }
 
-function PlayRound (getComputerChoice, getPlayerChoice ) {
-    const playerChoice = getPlayerChoice().toLowerCase();
-    const computerChoice = getComputerChoice();
+function PlayRound (computerChoice, playerChoice ) {
     
     if (playerChoice === computerChoice) {
         return "T";
@@ -35,31 +33,75 @@ function PlayRound (getComputerChoice, getPlayerChoice ) {
     }
 }
 
-function game () {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i ++) {
-        const result = PlayRound(getComputerChoice, getPlayerChoice);
-        if (result === 'W') {
-            playerScore ++;
-        }
-        else if (result === 'L') {
-            computerScore ++;
-        }
-        else {
-            console.log(`Round: ${i + 1} - It's a tie! Restarting the round.`);
-            i--;
-        }
+// function game () {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (let i = 0; i < 5; i ++) {
+//         const result = PlayRound(getComputerChoice, getPlayerChoice);
+//         if (result === 'W') {
+//             playerScore ++;
+//         }
+//         else if (result === 'L') {
+//             computerScore ++;
+//         }
+//         else {
+//             console.log(`Round: ${i + 1} - It's a tie! Restarting the round.`);
+//             i--;
+//         }
 
-        console.log(`Round: ${i + 1}`);
-        console.log(`Player Score: ${playerScore}`);
-        console.log(`Computer Score: ${computerScore}`);
+//         console.log(`Round: ${i + 1}`);
+//         console.log(`Player Score: ${playerScore}`);
+//         console.log(`Computer Score: ${computerScore}`);
+//     }
+
+//     if (computerScore > playerScore) {
+//         return "Computer Wins"
+//     }
+//     else {
+//         return "Player Wins"
+//     }
+// }
+
+const rockButton = document.getElementById("rock")
+const paperButton = document.getElementById("paper")
+const scissorButton = document.getElementById("scissor")
+
+// function imageSelector (value) {
+//     if (value === "rock") {
+//         return 
+//     }
+// }
+
+function updateImage (computerChoice, playerChoice) {
+    const container1 = document.querySelector(".human-box img");
+    const container2 = document.querySelector(".computer-box img");
+    
+    container1.src = `images/${playerChoice}.png`;
+    container2.src = `images/${computerChoice}.png`;
+
+    container1.setAttribute('style', 'width: 150px; height: 150px');
+    container2.setAttribute('style', 'width: 150px; height: 150px');
+}
+
+function clickFunction (playerChoice) {
+    const computerChoice = getComputerChoice();
+    updateImage(computerChoice, playerChoice);
+    const result = PlayRound(computerChoice, playerChoice);
+    console.log(result);
+    if (result === "W") {
+        const container = document.querySelector(".result-text");
+        container.textContent = "HUMAN WINS";
     }
-
-    if (computerScore > playerScore) {
-        return "Computer Wins"
+    else if (result === "L") {
+        const container = document.querySelector(".result-text");
+        container.textContent = "COMPUTER WINS";
     }
     else {
-        return "Player Wins"
+        const container = document.querySelector(".result-text");
+        container.textContent = "ITS A TIE";
     }
 }
+
+rockButton.addEventListener('click', () => {clickFunction("rock")})
+paperButton.addEventListener('click', () => {clickFunction("paper")})
+scissorButton.addEventListener('click', () => {clickFunction("scissor")})
